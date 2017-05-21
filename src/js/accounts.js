4ecbc11443e7a086fa33fb467beb8aea54c40604
Christian Grøngaard <christianhg@gmail.com>
2017-05-21 17:07:01 +0200

    Use react-router and render header and footer using react too

---
+++ src/js/accounts.js
@@ -0,0 +1,27 @@
+export const accounts = [
+  {
+    link: 'https://flickr.com/photos/christianhg',
+    name: 'flickr',
+    username: 'christianhg'
+  },
+  {
+    link: 'https://github.com/christianhg',
+    name: 'github',
+    username: 'christianhg'
+  },
+  {
+    link: 'https://last.fm/user/christianhg',
+    name: 'last.fm',
+    username: 'christianhg'
+  },
+  {
+    link: 'https://www.linkedin.com/in/christianhg',
+    name: 'linkedin',
+    username: 'christianhg'
+  },
+  {
+    link: 'https://twitter.com/christianhg',
+    name: 'twitter',
+    username: 'christianhg'
+  }
+]
--- src/js/main.js
+++ src/js/main.js
@@ -4,61 +4,119 @@ import '../main.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
 import { Parser } from 'html-to-react'
-
 import { articles, home, wrote } from './pages'
+import { accounts } from './accounts'
 
-const router = require('page')
 const parser = new Parser()
-
 const baseTitle = 'brummefar.dk'
-const main = document.getElementsByTagName('main')[0]
-const menu = document.getElementsByTagName('nav')[0]
-
-const menuLink = link => <li key={link.path}><a href={link.path}>{link.title}</a></li>
+const root = document.getElementById('root')
 const getTitle = pageTitle => `${pageTitle} ~ ${baseTitle}`
 
+const Account = account => (
+  <span key={account.link}>
+    <br />
+    <a href={account.link} rel='me'>{account.name}/<strong>{account.username}</strong></a>
+  </span>
+)
+
+const Footer = () => (
+  <footer>
+    <p>
+      &copy; 2017 ~ Christian 'Brummefar' Grøngaard
+      {accounts.map(Account)}
+    </p>
+  </footer>
+)
+
+const MenuLink = link => (
+  <li key={link.path}>
+    <Link to={link.path}>{link.title}</Link>
+  </li>
+)
+
 const Menu = props => (
-  <ul>
-    {props.page.links && props.page.links.map(menuLink)}
-  </ul>
+  <nav>
+    <ul>
+      {props.page.links && props.page.links.map(MenuLink)}
+    </ul>
+  </nav>
+)
+
+const Header = props => (
+  <header>
+    <h1><Link to='/'>{baseTitle}</Link></h1>
+    <Menu page={props.page} />
+  </header>
 )
 
 const Main = props => (
-  <div>
+  <main>
     {props.page.date && <div className='date'>{props.page.date}</div>}
     <div>{parser.parse(props.page.content)}</div>
-  </div>
+  </main>
 )
 
-const setPage = page => {
-  document.title = getTitle(page.link.title)
-  ReactDOM.render(<Menu page={page} />, menu)
-  page.getContent()
-    .then(content =>
-      ReactDOM.render(<Main page={Object.assign({}, page, { content })} />, main))
+class Page extends React.Component {
+  constructor (props) {
+    super()
+    this.state = {
+      page: Object.assign(props.page, { content: 'Loading...' })
+    }
+  }
+
+  componentDidMount () {
+    this.state.page.getContent()
+      .then(content => this.setState(prevState => Object.assign(prevState.page, { content })))
+  }
+
+  render () {
+    document.title = getTitle(this.state.page.link.title)
+
+    return (
+      <div>
+        <Header page={this.state.page} />
+        <Main page={this.state.page} />
+      </div>
+    )
+  }
 }
 
-router('/', () => {
-  setPage(home)
-})
+const Home = () => (
+  <Page page={home} />
+)
 
-router('/wrote', () => {
-  setPage(wrote)
-})
+const Wrote = () => (
+  <Page page={wrote} />
+)
 
-router('/wrote/:article', context => {
-  const article = articles.find(article => article.link.path === context.path)
+const Article = ({ match }) => {
+  const article = articles
+    .find(article => article.link.path === `/wrote/${match.params.article}`)
 
-  if (article) {
-    setPage(article)
-  } else {
-    router.redirect('/')
-  }
-})
+  return article
+    ? <Page page={article} />
+    : <Home />
+}
 
-router('*', () => {
-  router.redirect('/')
-})
+const Routes = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/wrote' component={Wrote} />
+        <Route path='/wrote/:article' component={Article} />
+        <Route component={Home} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
+)
 
-router()
+ReactDOM.render(<Routes />, root)
